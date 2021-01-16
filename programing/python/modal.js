99bb04831cd91491731213d36be8729ff881dfeb
norask8 <56714059+norask8@users.noreply.github.com>
2021-01-16 22:14:11 +0900

    Add files via upload

---
+++ programing/python/modal.js
@@ -0,0 +1,26 @@
+$(function() {
+  $('.janken').click(function() {
+    $('#janken-show').fadeIn();
+  });
+  $('#close-janken').click(function() {
+    $('#janken-show').fadeOut();
+  });
+});
+
+$(function() {
+  $('.ryouri').click(function() {
+    $('#ryouri-show').fadeIn();
+  });
+  $('#close-ryouri').click(function() {
+    $('#ryouri-show').fadeOut();
+  });
+});
+
+$(function() {
+  $('.sinx').click(function() {
+    $('#sinx-show').fadeIn();
+  });
+  $('#close-sinx').click(function() {
+    $('#sinx-show').fadeOut();
+  });
+});
